b024441441656fdf1fbfff0716bef50d6ce68cff
ShanuXo <shanusah8001@gmail.com>
2024-03-21 19:55:16 +0530

    Basic Function using Express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //Creating an http server -Using Express
+
+const express = require("express");
+
+const app=express();
+
+const calculateSum = (n) =>{
+    let sum=0;
+    for(let i=0;i<n;i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+app.get("/", function(req,res)
+{
+    const n= req.query.n;//to catch an query input we are using query
+    const ans=calculateSum(Number(n));
+    res.send("Sum of N numbers"+ ans);
+})
+
+app.listen(3001);
